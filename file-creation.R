05366408faae5d17b55889ad6ef0c5e87c5a37e8
Sara Colando <skca2020@mymail.pomona.edu>
2024-05-31 16:10:23 -0700

    adding docs

---
+++ file-creation.R
@@ -0,0 +1,4 @@
+# file creation
+
+list <- c("Privacy", "Causation", "Bias", "Explainability", "Democracy","Workplace",
+          "Predictive-Policing", "Alignment", "Responsibility", "Characterizations")
