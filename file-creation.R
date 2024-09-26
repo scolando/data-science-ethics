83b66fef2b3a1520f2ade31baf985e94fb1ac688
Sara Colando <97759116+scolando@users.noreply.github.com>
2024-09-26 13:30:27 -0400

    Delete file-creation.R

--- file-creation.R
+++
@@ -1,4 +0,0 @@
-# file creation
-
-list <- c("Privacy", "Causation", "Bias", "Explainability", "Democracy","Workplace",
-          "Predictive-Policing", "Alignment", "Responsibility", "Characterizations")
